2ae9209de585fefff44ddff8f2615e83cff881e2
Thu Phan <alicebe.phan@gmail.com>
2021-09-10 01:47:15 -0500

    color diplayed to indicate whether it is past, present or future

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,6 +1,31 @@
-//Setting the current date
-
+// Daclaring variables and assigning values to them for present moment
+var currentHour = moment().format("HH");
+console.log("HH", currentHour);
+// Setting the current date
 var today = moment();
-$("#currentDay").text(today.format("ddd, MMM Do, YYYY, hh:mm:ss"));
+$("#currentDay").text(today.format("ddd, MMM Do, YYYY"));
 
+
+
+// Using jQuery to manipulate the DOM
+// $("#timeblock").addClass("past");
+// $("#timeblock").addClass("present");
+// $("#timeblock").addClass("future");
+
+function displayColor() {
+    for (var i = 9; i < 18; i++) {
+        if (currentHour === i) {
+            // string concatenation
+            $("#" + i).addClass("present");
+
+        } else if (currentHour > i) {
+            $("#" + i).addClass("past");
+
+        } else {
+            $("#" + i).addClass("future");
+        }
+    }
+
+}
+displayColor();
 
